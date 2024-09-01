d11ddf84f7bf8adedecc724e26ad09dbcd4b6453
Kartikhub <sharmakartik66@gmail.com>
2024-09-01 23:36:32 +0530

    arguments in the cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let search_query = &args[1];
+    let file_path = &args[2];
+    
+    println!("query is {search_query}");
+    println!("Provided file path is {file_path}");
 }
