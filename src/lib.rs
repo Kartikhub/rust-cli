66935aaa67d8e8896e7ef84ebf4889fee7d9b7cc
Kartikhub <sharmakartik66@gmail.com>
2024-09-03 00:24:19 +0530

    search logic added

--- src/lib.rs
+++ src/lib.rs
@@ -21,8 +21,37 @@ impl Arguments {
 }
 
 pub fn run(cli_args: Arguments) -> Result<(), Box<dyn Error>> { 
-    let contents = fs::read_to_string(cli_args.file_path)?;
-    println!("{contents}");
 
+    let contents = fs::read_to_string(cli_args.file_path)?;
+
+    for (i, line) in search(&cli_args.search_query, &contents) {
+        println!("LINE: {} {}", i, line);
+    }
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<(usize , &'a str)> {
+    let mut result: Vec<(usize, &str)> = Vec::new();
+    for (i, line) in contents.lines().enumerate() {
+        if line.contains(query) {
+            result.push((i+1, line));
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_test() {
+        let query = "effi";
+        let contents = "\
+Rust:
+reliable, efficient, fearless.
+Choose all three.";
+
+        assert_eq!(vec![(2, "reliable, efficient, fearless.")], search(query, contents));
+    }
 }
