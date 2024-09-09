8a8273b4eb897632fc21bd5beee4eaafb1045fd5
Kartikhub <sharmakartik66@gmail.com>
2024-09-09 23:05:02 +0530

    Add error message to std error instead of std output

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let cli_args = Arguments::build(&args).unwrap_or_else(|err| {
-        println!("Problems parsing arguements: {err}");
+        eprintln!("Problems parsing arguements: {err}");
         process::exit(1);
     });
 
     if let Err(e) = rust_cli::run(cli_args) {
-        println!("Unable to read the file: {e}");
+        eprintln!("Unable to read the file: {e}");
         process::exit(1);
     }
 }
